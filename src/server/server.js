16300b7584d06ceb9a283a1f86b64b3bb7ec91dd
Pierre <Pierre.Mingels@protonmail.com>
2023-06-20 17:37:52 +0200

    changed build path in server file

--- src/server/server.js
+++ src/server/server.js
@@ -80,15 +80,15 @@ io.on('connection', (socket) => {
     });
 });
 
-app.use(express.static(path.join(__dirname, '../../../build')));
+app.use(express.static(path.join(__dirname, '../../build')));
 console.log("Build path:", path.join(__dirname, '../../../build'));
 
 app.get('*', (req, res) => {
   console.log("Received request for", req.originalUrl);
-  res.sendFile(path.join(__dirname, '../../../build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
 
-fs.readdir(path.join(__dirname, '../../../build'), (err, files) => {
+fs.readdir(path.join(__dirname, '../../build'), (err, files) => {
   console.log("Build directory contents:", files);
 });
   
