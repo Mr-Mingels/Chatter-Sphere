4ad8c777567f4a9fd78a45a2f52707e74b3599d8
Pierre <Pierre.Mingels@protonmail.com>
2023-06-21 23:13:20 +0200

    updated my vercel.json file

--- api/server.js
+++ api/server.js
@@ -93,6 +93,12 @@ app.get('/', (req, res) => {
   }
 })
 
+app.use(express.static(path.join(__dirname, '../../build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
+
 http.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
