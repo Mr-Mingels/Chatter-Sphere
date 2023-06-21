e716893e81a89d53acbc9a591d19ab0e90839f4f
Pierre <Pierre.Mingels@protonmail.com>
2023-06-21 22:55:28 +0200

    deleted and installed a different version of bcrypt

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const Chat = require('../models/chat');
 
