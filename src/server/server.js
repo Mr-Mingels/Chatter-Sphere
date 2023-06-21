71ac8d0de0cf8529d75e85ea4c3b11d647f64a17
Pierre <Pierre.Mingels@protonmail.com>
2023-06-21 11:17:07 +0200

    test

--- src/server/server.js
+++ src/server/server.js
@@ -80,6 +80,14 @@ app.use(authRoutes);
 app.use(chatsRoutes)
 app.use(groupRoutes);
 app.use(friendsRoutes)
+  
+app.get('/', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.json(req.user)
+  } else {
+    res.status(401).send('Unauthorized');
+  }
+})
 
 app.use(express.static(path.join(__dirname, '../../build')));
 
