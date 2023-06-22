3a80eb57d1018128cb8e85a81d5ebd84db326b0d
Pierre <Pierre.Mingels@protonmail.com>
2023-06-22 12:49:45 +0200

    testing deployment

--- src/server/server.js
+++ src/server/server.js
@@ -92,6 +92,12 @@ app.get('/', (req, res) => {
     res.status(401).send('Unauthorized');
   }
 })
+/*
+ app.use(express.static(path.join(__dirname, '../../build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+}); */
 
 http.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
